c21cd154d915f360b5d04055013f1c31b0677572
hyzhu <zhuhuiyu2499669698@163.com>
2025-06-10 14:31:01 +0800

    完成告警管理后端接口

---
+++ maku-cloud-new/menu/t_message_menu.sql
@@ -0,0 +1,13 @@
+
+-- 初始化菜单
+INSERT INTO sys_menu (pid, name, url, authority, type, open_style, icon, sort, version, deleted, creator, create_time, updater, update_time) VALUES (1, '告警管理', 'iot/message/index', NULL, 0, 0, 'icon-menu', 0, 0, 0, 10000, now(), 10000, now());
+
+
+set @menuId = @@identity;
+
+INSERT INTO sys_menu (pid, name, url, authority, type, open_style, icon, sort, version, deleted, creator, create_time, updater, update_time) VALUES ((SELECT @menuId), '查看', '', 'iot:message:page', 1, 0, '', 0, 0, 0, 10000, now(), 10000, now());
+INSERT INTO sys_menu (pid, name, url, authority, type, open_style, icon, sort, version, deleted, creator, create_time, updater, update_time) VALUES ((SELECT @menuId), '新增', '', 'iot:message:save', 1, 0, '', 1, 0, 0, 10000, now(), 10000, now());
+INSERT INTO sys_menu (pid, name, url, authority, type, open_style, icon, sort, version, deleted, creator, create_time, updater, update_time) VALUES ((SELECT @menuId), '修改', '', 'iot:message:update,iot:message:info', 1, 0, '', 2, 0, 0, 10000, now(), 10000, now());
+INSERT INTO sys_menu (pid, name, url, authority, type, open_style, icon, sort, version, deleted, creator, create_time, updater, update_time) VALUES ((SELECT @menuId), '删除', '', 'iot:message:delete', 1, 0, '', 3, 0, 0, 10000, now(), 10000, now());
+INSERT INTO sys_menu (pid, name, url, authority, type, open_style, icon, sort, version, deleted, creator, create_time, updater, update_time) VALUES ((SELECT @menuId), '导入', '', 'iot:message:import', 1, 0, '', 3, 0, 0, 10000, now(), 10000, now());
+INSERT INTO sys_menu (pid, name, url, authority, type, open_style, icon, sort, version, deleted, creator, create_time, updater, update_time) VALUES ((SELECT @menuId), '导出', '', 'iot:message:export', 1, 0, '', 3, 0, 0, 10000, now(), 10000, now());
